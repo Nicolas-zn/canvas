94e3112cd96c9b4327fc0b2d76fc7793203a4b2e
nicomacbookpro <805879871@qq.com>
2024-12-26 13:29:41 +0800

    提取application_list

---
+++ src/application_list.ts
@@ -0,0 +1,92 @@
+
+let application_list = [
+    {
+        component_name: 'parking', application_name: '停车位'
+    },
+    {
+        component_name: 'brick', application_name: '红砖'
+    },
+    {
+        component_name: 'arrow', application_name: '箭头'
+    },
+    {
+        component_name: 'cross', application_name: '十字架'
+    },
+    {
+        component_name: 'dolby', application_name: '杜比音效'
+    },
+    {
+        component_name: 'traffic_limit_speed', application_name: '交通-限速'
+    },
+    {
+        component_name: 'traffic_forbid_driveIn', application_name: '交通-禁止驶入'
+    },
+    {
+        component_name: 'traffic_yield', application_name: '交通-停车让行'
+    },
+    {
+        component_name: 'traffic_slow_yield', application_name: '交通-减速让行'
+    },
+    {
+        component_name: 'traffic_limit_height', application_name: '交通-限高'
+    },
+    {
+        component_name: 'traffic_limit_width', application_name: '交通-限宽'
+    },
+    {
+        component_name: 'traffic_forbid_park', application_name: '交通-禁止停车'
+    },
+    {
+        component_name: 'traffic_slow_drive', application_name: '交通-慢行'
+    },
+    {
+        component_name: 'traffic_highway', application_name: '交通-高速标识'
+    },
+    {
+        component_name: 'traffic_forbid_access', application_name: '交通-禁止通行'
+    },
+    {
+        component_name: 'traffic_forbid_straight', application_name: '交通-禁止直行'
+    },
+    {
+        component_name: 'traffic_forbid_UTurn', application_name: '交通-禁止掉头'
+    },
+    {
+        component_name: 'traffic_turn_left', application_name: '交通-左转'
+    },
+    {
+        component_name: 'traffic_road_sign', application_name: '交通-指示牌'
+    },
+    {
+        component_name: 'license_plate', application_name: '立体车牌号'
+    },
+    {
+        component_name: 'boc_log', application_name: '中国银行'
+    },
+    {
+        component_name: 'tai_chi', application_name: '太极'
+    },
+    {
+        component_name: 'security_camera', application_name: '监控摄像头'
+    },
+    {
+        component_name: 'car_logo_benz', application_name: '车标-奔驰'
+    },
+    {
+        component_name: 'car_logo_audi', application_name: '车标-奥迪'
+    },
+    {
+        component_name: 'car_logo_bmw', application_name: '车标-宝马'
+    },
+    {
+        component_name: 'car_logo_toyota', application_name: '车标-丰田'
+    },
+    {
+        component_name: 'car_logo_vw', application_name: '车标-大众'
+    },
+    {
+        component_name: 'car_logo_nissan', application_name: '车标-日产'
+    },
+]
+
+export default application_list
